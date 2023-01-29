7fd8e0f126fc78bb8d56b1b9d9819a86b57e6e24
NishantSingh02 <nst7372@gmail.com>
2023-01-29 18:43:13 +0530

    ListNode methods added

---
+++ src/LinkedList/DoublyLinkedList/ListNode.java
@@ -0,0 +1,22 @@
+package LinkedList.DoublyLinkedList;
+
+import java.util.List;
+
+public class ListNode {
+    int val;
+    ListNode prev, next;
+    public ListNode(int val){
+        this.val = val;
+    }
+    public ListNode(int val, ListNode prev, ListNode next) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+    @Override
+    public String toString() {
+        if(this.next==null)
+            return (this.val)+"->";
+        return (this.val)+"->"+ this.next;
+    }
+}
