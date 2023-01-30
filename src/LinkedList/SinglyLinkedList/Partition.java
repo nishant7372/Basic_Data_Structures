b08e72f644891ac74d64f8f35525d33b32a218d7
NishantSingh02 <nst7372@gmail.com>
2023-01-30 15:18:21 +0530

    Partiton methods Updated

--- src/LinkedList/SinglyLinkedList/Partition.java
+++ src/LinkedList/SinglyLinkedList/Partition.java
@@ -38,8 +38,8 @@ public class Partition {
     The length of each part should be as equal as possible:
     no two parts should have a size differing by more than one.
     This may lead to some parts being null.
-    The parts should be in the order of occurrence in the input list,
-    and parts occurring earlier should always have a size greater than
+    The parts should be in the order of occurrence in the input list, and parts
+    occurring earlier should always have a size greater than
     or equal to parts occurring later.
     Return an array of the k parts.
     */
