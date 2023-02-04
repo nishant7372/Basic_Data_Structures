b03c4f8e6655a79f3e17ac4a22a0e39c818edd92
NishantSingh02 <nst7372@gmail.com>
2023-02-05 01:47:50 +0530

    Queue Main Created

---
+++ src/Array/Main.java
@@ -0,0 +1,10 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Queue/Main.java
@@ -0,0 +1,9 @@
+package Queue;
+
+import java.util.Scanner;
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
