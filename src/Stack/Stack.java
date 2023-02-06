dbcfa5e08de234a3ef36555aefdbf44ee134dc5c
NishantSingh02 <nst7372@gmail.com>
2023-02-06 14:26:09 +0530

    Stack Methods Updated

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -3,44 +3,41 @@ package Stack;
 public class Stack {
     Integer[] st;
     int top;
-
     public Stack(int size){
         this.st = new Integer[size];
         this.top = -1;
     }
-    public void push(int val){
-        if(!isFull())
-            st[++top]=val;
+    public boolean push(int val){
+        if(!isFull()) {
+            st[++top] = val;
+            return true;
+        }
+        return false;
     }
-
     public Integer pop(){
         if(!isEmpty()){
             return st[top--];
         }
         return null;
     }
-
     public Integer peek(){
         if(!isEmpty()){
             return st[top];
         }
         return null;
     }
-
     public boolean isEmpty(){
         return top==-1;
     }
-
     public boolean isFull(){
         return top==st.length-1;
     }
-
     public void print() {
         for(int i=Math.max(top,Math.min(st.length-1,5));i>=0;i--) {
             if(i>top)
                 System.out.println("|           |");
             else {
-                int len = 9-length(st[i]+"");
+                int len = 9-Integer.toString(st[i]).length();
                 int[] space = new int[2];
                 if(len%2==0){
                     space[0]=space[1]=len/2;
@@ -53,9 +50,5 @@ public class Stack {
             }
             System.out.println("|-----------|");
         }
-    }
-
-    private int length(String s){
-        return s.length();
     }
 }
