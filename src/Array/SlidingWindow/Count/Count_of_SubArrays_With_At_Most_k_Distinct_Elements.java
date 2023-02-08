468e426974ab28f88b47ce6946118b84d013c7b6
NishantSingh02 <nst7372@gmail.com>
2023-02-08 20:02:32 +0530

    Methods Updated

--- src/Array/SlidingWindow/Count/Count_of_SubArrays_With_At_Most_k_Distinct_Elements.java
+++ src/Array/SlidingWindow/Count/Count_of_SubArrays_With_At_Most_k_Distinct_Elements.java
@@ -1,10 +1,12 @@
-package Array.SlidingWindow;
+package Array.SlidingWindow.Count;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Count_of_SubArrays_With_At_Most_k_Distinct_Elements {
-    public int countOfSubArraysWithAtMostKDistinctElements(int[] nums,int k){
+
+    // 1. HashTable for smaller constraints +ves only
+    public int countOfSubArraysWithAtMostKDistinctElements_Table(int[] nums,int k){
         int[] freq = new int[nums.length]; //0 <= nums[i] < nums.length
         int j=0, count=0, unique=0;
         for(int i=0;i<nums.length;i++){
@@ -19,7 +21,9 @@ public class Count_of_SubArrays_With_At_Most_k_Distinct_Elements {
         }
         return count;
     }
-    public int countOfSubArraysWithAtMostKDistinctElements2(int[] nums,int k){
+
+    // 2. HashMap for larger constraints including -ves
+    public int countOfSubArraysWithAtMostKDistinctElements_Map(int[] nums,int k){
         Map<Integer,Integer> map = new HashMap<>();
         int j=0, max=0, count=0;
         for(int i=0;i<nums.length;i++){
