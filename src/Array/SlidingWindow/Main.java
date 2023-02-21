118f3a5f99c090a4cf00b0437741e57999804e38
NishantSingh02 <nst7372@gmail.com>
2023-02-21 20:10:42 +0530

    Created

---
+++ src/Array/SlidingWindow/Main.java
@@ -0,0 +1,10 @@
+package Array.SlidingWindow;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
